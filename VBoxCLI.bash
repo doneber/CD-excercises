2a4f84afc11aeade03f16ccb11fd0ffad5311339
doneber <doneber98@gmail.com>
2021-01-20 14:25:39 -0300

    Added turn on & turn off VM

--- VBoxCLI.bash
+++ VBoxCLI.bash
@@ -1,18 +1,17 @@
 clear
 VBoxManage="/mnt/d/Program\ Files/Oracle/VirtualBox/VBoxManage.exe"
-
-echo "***********************************"
 echo "********   WELCOME   **************"
-echo "***********************************"
 echo "What do you wanna do?"
 echo "    1.- List the Virtual Machines (vms)"
-echo "    2.- Create an especific VM"
-echo "    3.- Delete an especific VM"
-echo "    4.- Quit"
+echo "    2.- Start a VM"
+echo "    3.- Turn off a VM"
+echo "    4.- Create a VM"
+echo "    5.- Delete a VM"
+echo "    6.- Quit"
 echo "Write an option"
 echo ""
 read option
-while [ $option -lt 4 ]
+while [ $option -lt 6 ]
 do
   case $option in
 
@@ -22,12 +21,22 @@ do
       eval $VBoxManage list vms
       ;;
     2)
+      echo "Type your VM to turn on"
+      read vm
+      eval "$VBoxManage startvm $vm "
+      ;;
+    3)
+      echo "Type your VM to turn off"
+      read vm
+      eval "$VBoxManage controlvm $vm poweroff vm soft"
+      ;;
+    4)
       echo "You choose option 2"
       echo "What will be the VM's name?"
       read name
       eval $VBoxManage createvm --name "VM-$name"
       ;;
-    3)
+    5)
       echo "You choose option 3"
       echo "What is the VM's uid or name?"
       read vm
@@ -43,11 +52,13 @@ do
   read intro
   clear
   echo ""
-  echo "do you wanna do something else?"
+  echo "Do you wanna do something else?"
   echo "    1.- List the Virtual Machines (vms)"
-  echo "    2.- Create an VM"
-  echo "    3.- Delete an VM"
-  echo "    4.- Quit"
+  echo "    2.- Start a VM"
+  echo "    3.- Turn off a VM"
+  echo "    4.- Create a VM"
+  echo "    5.- Delete a VM"
+  echo "    6.- Quit"
   echo "Write an option"
   echo ""
   read option
