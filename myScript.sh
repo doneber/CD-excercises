3b7e54c1e9d26ab32ec8bd78b35b392041277a0d
doneber <doneber98@gmail.com>
2021-01-21 10:50:25 -0300

    Update script to CRUD VMs

--- myScript.sh
+++ myScript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 typeset -A config # init array
-VBoxManage="/mnt/d/Program\ Files/Oracle/VirtualBox/VBoxManage.exe"
+config=( # set default values in config array
+    [VBoxManage]="/mnt/d/Program\ Files/Oracle/VirtualBox/VBoxManage.exe"
+    [vmname]="vm-efe"
+)
 if [ -t 0 ]; then
     echo
   # if [ $# -eq 0 ]; then
@@ -16,38 +19,60 @@ else
       then
           varname=$(echo "$line" | cut -d '=' -f 1)
           config[$varname]=$(echo "$line" | cut -d '=' -f 2-)
+          echo $varname
+          echo $config
       fi
   done
+  if echo $line | grep -F = &>/dev/null
+  then
+    varname=$(echo "$line" | cut -d '=' -f 1)
+    config[$varname]=$(echo "$line" | cut -d '=' -f 2-)
+  fi
 fi
 case "$1" in
   -h|--help)
     echo "usage: $0"
     echo "        [--help] [--list]"
-    echo "        [create < fileConfig] [delete < fileConfig]"
     echo "        [start < fileConfig] [shoutdown < fileConfig]"
+    echo "        [create < fileConfig] [delete < fileConfig]"
     echo
     echo "These are common commands used:"
     echo
+    echo "        start < fileNameConfig                    Start a VM with input config file"
+    echo "        shoutdown < fileNameConfig                Shoutdown a VM with input config file"
     echo "        create < fileNameConfig                   Create a VM with input config file"
     echo "        delete < fileNameConfig                   Delete a VM with input config file"
     exit 0
   ;;
   -l|--list)
-    eval $VBoxManage list vms
+    eval ${config[VBoxManage]} list vms
+  ;;
+  start)
+    if [ -t 0 ]; then
+      echo "You need an input config file to Start a VM"
+    else
+      eval ${config[VBoxManage]} startvm ${config[vmname]}
+    fi
+  ;;
+  shoutdown)
+    if [ -t 0 ]; then
+      echo "You need an input config file to SHOUTDOWN a VM"
+    else
+      eval "${config[VBoxManage]} controlvm ${config[vmname]} poweroff vm soft"
+    fi
   ;;
   create)
     if [ -t 0 ]; then
       echo "You need an input config file to CREATE a VM"
     else
-      echo ${config[username]}
-      echo ${config[password]}
+      eval ${config[VBoxManage]} createvm --name ${config[vmname]}
     fi
   ;;
   delete)
     if [ -t 0 ]; then
       echo "You need an input file config to DELETE a VM"
     else
-      echo ${config[name]}
+    eval ${config[VBoxManage]} unregistervm ${config[vmname]} --delete
     fi
   ;;
   *)
