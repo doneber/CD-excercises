302904e5423a7fda1c57cdafb4c183bb4af0b57c
doneber <doneber98@gmail.com>
2021-01-28 17:56:10 -0400

    Add webpack build from ts to js file

--- ExpressTs/webpack.config.js
+++ ExpressTs/webpack.config.js
@@ -2,11 +2,21 @@ const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'app.ts'),
-  mode: 'production',
+  mode: 'development',
   target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          'ts-loader',
+        ]
+      }
+    ]
   },
   resolve: {
     extensions: ['.ts', '.js'],
